4f29f46e57c88e1d1051fad3342da011e1cdfad0
Abu Jubaer <18-38784-3@student.aiub.edu>
2021-07-05 14:08:04 +0600

    Create index.js

---
+++ JS Basic/index.js
@@ -0,0 +1,60 @@
+"use strict"
+
+//a = 0;
+//var b= 10;
+let c=10;
+const e =10;
+
+//let student = [1,3,4,'abc'];
+//console.log(student[1]);
+
+/*function sum(a,b){
+	return a+b;
+}*/
+
+/*const sum = function(a, b){
+	return a+b;
+}*/
+
+
+/*const sum = (a, b)=>{
+	return a+b;
+}*/
+
+/*const sum = (a, b)=> a+b;
+console.log(sum(3,5));*/
+//console.log(`this is test value: ${c==='10'? 'true':'false'} `);
+
+const student = {
+	id: 1,
+	name: 'abc',
+	dept: 'SE',
+	getName: function(){
+		return this.name;
+	},
+	getObj: function (){
+		return {
+			version: '1.0',
+			getVersion: function(){
+				return this.version;
+			}
+		}
+	}
+}
+//student.name = 'xyz';
+//console.log(student['getName']());
+
+//console.log(student.getObj().getVersion());
+console.log(student['getObj']()['getVersion']());
+
+const student = ()=>{
+	return {
+		id: 1,
+		name: 'abc',
+		dept: 'SE'
+	}
+}
+
+const s1 = student();
+const s2 = student();
+const s3 = student();
