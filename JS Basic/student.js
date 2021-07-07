8cc3e98697b29c874a352d2d68f03512459f5115
Abu Jubaer <18-38784-3@student.aiub.edu>
2021-07-07 14:03:38 +0600

    ES6 Basic

---
+++ JS Basic/student.js
@@ -0,0 +1,19 @@
+/*const student = {
+	id: 1,
+	name: 'alamin',
+	getName: function(){
+		return this.name;
+	}
+}*/
+
+const name = "XYZ";
+const student = function(){
+	return {
+		id: 1,
+		name: 'alamin',
+		getName: function(){
+			return this.name;
+		}
+	}
+}
+module.exports.std = student;
