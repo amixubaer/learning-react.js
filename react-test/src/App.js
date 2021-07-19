624de0c7a46c59ebacd1816befc3f225b249c92e
Abu Jubaer <18-38784-3@student.aiub.edu>
2021-07-19 13:56:48 +0600

    simple react app

--- react-test/src/App.js
+++ react-test/src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react';
+import UserList from './components/UserList';
+import {users} from './usersData';
 
 function App() {
+  
+  const [myuser, setUsers] = useState(users);
+
+  const deleteCallback = (id)=>{
+    const data = myuser.filter((user)=>user.id !== id);
+    setUsers(data);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div >
+        <UserList list={myuser} callback={deleteCallback}/>
     </div>
   );
 }
